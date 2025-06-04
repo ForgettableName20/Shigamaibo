8cdde08684bd5f6ff3d45888b485ce8943b3384f
GoncaloQuiterio <8075299@formacao.iefp.pt>
2025-06-04 17:47:10 +0100

    port changed at the bottom of index.js

--- index.js
+++ index.js
@@ -19,9 +19,6 @@ const db = mysql.createPool(
     });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
 
 db.getConnection((err, connection) =>
 {
@@ -203,7 +200,6 @@ app.get('/test', (req, res) =>
     });
 });
 
-app.listen(3000, () =>
-{
-    console.log('Server is running on http://localhost:3000');
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
